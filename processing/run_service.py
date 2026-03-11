e0272e212f8e9e52412653211ddf77cc9a9ae482
JonasPischke <pischke@mundialis.de>
2026-03-11 14:36:16 +0100

    added script for filtering and process chain

---
+++ processing/run_service.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+############################################################################
+# MODULE:      start_processing
+# AUTHOR(S):   Jonas Pischke
+#
+# PURPOSE:     Run complete service
+#
+# SPDX-FileCopyrightText: (c) 2025 by mundialis GmbH & Co. KG
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+############################################################################
+
+import json
+import time
+from pathlib import Path
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+### CONFIG ###
+
+GRASS_PROJECT = "athen_urban-green_epsg32634"
+START = "2025-12-01"
+END = "2025-12-05"
+TILE_ID = "34SGH"
+MAX_CLOUD_COVER = 100 # set max cloud cover
+MAIN_PC_PATH = "templates/"
+S2_ID_PROCESS_CHAIN = "pc_filter_S2.json"
+MAIN_PROCESS_CHAIN = "pc_loop_ids.json"
+
+# variables to set the actinia host, version, and user
+actinia_baseurl = "http://localhost:8088"
+actinia_version = "v3"
+actinia_url = actinia_baseurl + "/api/" + actinia_version
+# Todo: Get credentials from .env
+actinia_auth = HTTPBasicAuth("actinia", "actinia")  
+actinia_endpoint = f"{actinia_url}/locations/{GRASS_PROJECT}/processing_export"
+
+### functions ###
+
+# helper function to print formatted JSON using the json module
+class HasBeenTerminatedError(Exception):
+    """Throw exception class."""
+
+    def __init__(self, request_url) -> None:
+        """Throw exception."""
+        super().__init__(f"The resource <{request_url}> has been terminated.")
+
+# helper function to print formatted JSON using the json module
+def print_as_json(data) -> None:
+    """Print request as json."""
+    print(json.dumps(data, indent=2))
+
+
+# helper function to verify a request
+def verify_request(request, success_code=200) -> None:
+    """Verify the request."""
+    if request.status_code != success_code:
+        print(
+            "ERROR: actinia processing failed with status code "
+            f"{request.status_code}!",
+        )
+        print("See errors below:")
+        print_as_json(request.json())
+        request_url = request.json()["urls"]["status"]
+        requests.delete(url=request_url, auth=actinia_auth, timeout=20)
+        raise HasBeenTerminatedError(request_url)
+
+# function to make a POST request
+def post_request(request_url, actinia_auth, process_chain):
+    # make the POST request to start the processing
+    request = requests.post(
+        url=request_url, auth=actinia_auth, json=process_chain, timeout=20,
+    )
+    # check if anything went wrong
+    verify_request(request, 200)
+    # get a json-encoded content of the response
+    json_response = request.json()   
+    # get status request URL
+    status_request_url = json_response["urls"]["status"]
+
+    return json_response, status_request_url
+
+# function to make a GET request
+def get_request(request_url, actinia_auth):
+    request = requests.get(
+            url=request_url, auth=actinia_auth,
+            timeout=20,
+        )
+    verify_request(request, 200)
+    json_response = request.json()
+    return json_response
+
+### MAIN ###
+def main():
+    print("======================================")
+    print("Start Sentinel-2 processing for Athens...")
+    print("Using actinia at:")
+    print(actinia_endpoint)
+    print("======================================")
+    print("Filter Sentinel-2 scenes...")
+    print("======================================")
+
+    # make a POST request to the actinia data API
+    request_url = f"{actinia_url}/locations/{GRASS_PROJECT}/processing_export"
+
+    print("actinia POST request:")
+    print(request_url)
+    print("---")
+
+    # open process chain for S2 ID filtering
+    with open(MAIN_PC_PATH + "pc_filter_S2.json", encoding="utf-8") as file:
+        process_chain = json.load(file)
+
+    # send POST request for S2 ID filtering
+    _json_response, status_request_url = post_request(actinia_endpoint, actinia_auth, process_chain)
+
+    # wait for a few seconds (otherwise the status request might fail)
+    time.sleep(5)
+
+    # get status response as json
+    status_response = get_request(status_request_url.replace("https", "http"), actinia_auth)
+
+    # extract S2 IDs from status response
+    stdout = status_response["process_log"][0]["stdout"]
+    S2_scenes_dict = json.loads(stdout)
+    print(f"Found <{len(S2_scenes_dict)}> Sentinel-2 scene IDs:")
+    for _i, s2_id in S2_scenes_dict.items():
+        print(f" - {s2_id}")
+
+    print("======================================")
+    print("Start Download and Processing...")
+    print("======================================")
+
+    # load the main process chain
+    with open(MAIN_PC_PATH + MAIN_PROCESS_CHAIN, encoding="utf-8") as f:
+        process_chain = json.load(f)
+
+    # insert Sentinel-2 IDs into the process chain
+    process_chain["list"][1]["inputs"][0]["value"] = list(S2_scenes_dict.values())
+
+    # make the POST request to start the processing
+    json_response, status_request_url = post_request(actinia_endpoint, actinia_auth, process_chain)
+
+    # keep polling the status until finished
+    while json_response["status"] in {"accepted", "running"}:
+        status_response = get_request(status_request_url.replace("https", "http"), actinia_auth)
+        print(f"Polling status: {status_response['status']}")
+        print(f"Status request URL: {status_request_url}")
+        print(f"Doing: {status_response['message']}")
+        print("======================================")
+        time.sleep(30)
+
+    print(f"Final status: {status_response['status']}")
+    print(f"Processing finished for:")
+    for s2_id in list(S2_scenes_dict.values()):
+        print(f" - {s2_id}")
+    print("======================================")
+
+
+    # print output URLs
+    # Probably not necessary for final script
+    # Useful for dev status
+    for tif in status_response["urls"]["resources"]:
+        print(f"{Path(tif).name}: {tif.replace('https', 'http')}")
+
+if __name__ == "__main__":
+    main()
